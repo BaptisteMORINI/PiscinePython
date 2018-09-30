300eaf25dc95d49b25222cff5bb97bdd8390791d
BaptisteMORINI <43676769+BaptisteMORINI@users.noreply.github.com>
2018-09-30 23:14:39 +0200

    Update CalendrierGrégorien.py

--- CalendrierGrégorien.py
+++ CalendrierGrégorien.py
@@ -20,13 +20,13 @@ def anneeBissextile(annee):
 # Fonction pour saisir la date et la mettre dans le tableau D
 def dateSaisie():
     while True:
-        jour = int(input("Quel jour: "))                                                                                # On entre le jour
-        D.append(jour)                                                                                                  # On l'ajoute dans le tableau D
-        mois = int(input("Quel mois (en chiffre): "))                                                                   # idem pour le mois 
+        jour = int(input("Quel jour: "))                                                                                        # On entre le jour
+        D.append(jour)                                                                                                          # On l'ajoute dans le tableau D
+        mois = int(input("Quel mois (en chiffre): "))                                                                           # idem pour le mois 
         D.append(mois)
-        annee = int(input("De quelle année: "))                                                                         # idem pour l'année
+        annee = int(input("De quelle année: "))                                                                                 # idem pour l'année
         D.append(annee)
-        if not checkDate(D):                                                                                            # Vérification que la date rentrée est valide
+        if not checkDate(D):                                                                                                    # Vérification que la date rentrée est valide
             print("Erreur dans la saisie de la date")
         else:
             break
@@ -34,7 +34,7 @@ def dateSaisie():
 
 # Fonction pour vérifier que la date saisie est correcte
 def checkDate(D):
-    if anneeBissextile(D[2]):                                                                                           # On teste pour savoir si l'année est bissextile afin de pouvoir determiner le nombre de jour de février
+    if anneeBissextile(D[2]):                                                                                                   # On teste pour savoir si l'année est bissextile afin de pouvoir determiner le nombre de jour de février
         joursMois[1] = 29
     else:
         joursMois[1] = 28
@@ -43,15 +43,15 @@ def checkDate(D):
 # Fonction pour calculer le jour recherché selon la méthode donnée
 def calculJour(D):
 
-    x = D[2] % 100                                                                                                      # On garde les deux derniers chiffre de l'année
-    x = x + (x//4)                                                                                                      # On ajoute le quart de ce chiffre sans les restes
-    x = x + D[0]                                                                                                        # On ajoute la journée du mois
-    x = x + ajoutMois[D[1]-1]                                                                                           # Selon le mois on ajoute une des valeurs contenues dans ajoutMois
-    if anneeBissextile(D[2]) and (D[1] == 1 or D[1] == 2):                                                              # Si l'année est bissextile et que le mois est janvier ou février on ôte 1
+    x = D[2] % 100                                                                                                              # On garde les deux derniers chiffre de l'année
+    x = x + (x//4)                                                                                                              # On ajoute le quart de ce chiffre sans les restes
+    x = x + D[0]                                                                                                                # On ajoute la journée du mois
+    x = x + ajoutMois[D[1]-1]                                                                                                   # Selon le mois on ajoute une des valeurs contenues dans ajoutMois
+    if anneeBissextile(D[2]) and (D[1] == 1 or D[1] == 2):                                                                      # Si l'année est bissextile et que le mois est janvier ou février on ôte 1
         x = x - 1
-    x = x + ajoutSiecle[D[2]//100-16]                                                                                   # Selon le siècle on ajoute un des éléments de ajoutSiecle
-    x = x % 7                                                                                                           # On divise la somme obtenue par 7 et on garde le reste
-    return x                                                                                                            # Retourne une valeur entre 0 et 6 qui permettra de déterminer le jour cherché
+    x = x + ajoutSiecle[D[2]//100-16]                                                                                           # Selon le siècle on ajoute un des éléments de ajoutSiecle
+    x = x % 7                                                                                                                   # On divise la somme obtenue par 7 et on garde le reste
+    return x                                                                                                                    # Retourne une valeur entre 0 et 6 qui permettra de déterminer le jour cherché
 
 # Fonction pour afficher le résultat
 def affichage(D, joursVoulu):
